00137ab3b41fd43bd0a19c0f6cf25845db09fb10
techmindsetlab <79957730+techmindsetlab@users.noreply.github.com>
2025-02-03 18:29:10 +0700

    Update questionnaire-section.tsx

--- src/components/section/questionnaire-section.tsx
+++ src/components/section/questionnaire-section.tsx
@@ -6,28 +6,24 @@ const QuestionnaireSection = () => {
             {/* Cards */}
             <Link
                 to="https://app.sli.do/event/tdBsNfLTRKJfrFYP6HUoyZ"
-                target="_blank"
                 className="cursor-none bg-white rounded-md p-4 mb-3 lg:mr-6 border w-full text-center"
             >
                 Farewell Notes for Ravi Pokhriyal
             </Link>
             <Link
                 to="https://app.sli.do/event/96dqiQoFiyRZzAKxyd6s2i"
-                target="_blank"
                 className="cursor-none bg-white rounded-md p-4 mb-3 lg:mr-6 border w-full text-center"
             >
                 Q&A Pre Event Meeting Question
             </Link>
             <Link
                 to="https://app.sli.do/event/dPfLvSSkLLG8fgZBpfMTgX"
-                target="_blank"
                 className="cursor-none bg-white rounded-md p-4 mb-3 lg:mr-6 border w-full text-center"
             >
                 Q&A Country Meet 2025
             </Link>
             <Link
                 to="https://interprefy.interpret.world/loginlink?token=ofimeeting"
-                target="_blank"
                 className="cursor-none bg-white rounded-md p-4 mb-3 border w-full text-center"
             >
                 Audio Translation
